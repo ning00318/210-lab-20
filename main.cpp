8c4ff43bb4886b241ca5fdff685d2b2425f1d270
NINA <ninatsao@ningningning.lan>
2024-10-09 22:22:13 -0700

    6th commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// COMSC-210 | Lab 20: Chair Maker 3000 | Nina Tsao
+
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>  // rand()
@@ -57,13 +59,11 @@ int main() {
 
     //creating pointer to first chair object
     Chair *chairPtr = new Chair;
-    //chairPtr->setLegs(4);
-    //chairPtr->setPrices(121.21, 232.32, 414.14);
     chairPtr->print();
 
     //creating dynamic chair object with constructor
     double p[SIZE] = {525.25, 434.34, 252.52};
-    
+
     Chair *livingChair = new Chair(3, p);
     livingChair->print();
     delete livingChair;
@@ -71,13 +71,6 @@ int main() {
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
 
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
